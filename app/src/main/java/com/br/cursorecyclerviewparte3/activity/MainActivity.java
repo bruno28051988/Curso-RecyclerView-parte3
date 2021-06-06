d1ffda3a22722f367aa547d8673aa6332f26109c
Bruno Rodrigues Bordin <bbordin@server01.foursys.local>
2021-06-06 12:27:00 -0300

    update: subindo arquivo

---
+++ app/src/main/java/com/br/cursorecyclerviewparte3/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package com.br.cursorecyclerviewparte3.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.br.cursorecyclerviewparte3.R;
+import com.br.cursorecyclerviewparte3.adapter.Adapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Configurar Adapter
+        Adapter adapter = new Adapter();
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+    }
+}
